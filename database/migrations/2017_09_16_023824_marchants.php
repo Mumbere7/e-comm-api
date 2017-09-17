2c9b26d0202d60f07986ed0d4946d8cf9672f214
Mumbere Arnold <arnoldnzibuk@gmail.com>
2017-09-17 16:57:15 +0300

    just added a few fields

---
+++ database/migrations/2017_09_16_023824_marchants.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Marchants extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('marchants',  function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('accountType');
+            $table->string('businessName');
+            $table->string('country');
+            $table->string('city');
+            $table->integer('phone');
+            $table->string('email');
+            $table->string('paymentMethods');
+            $table->string('addrss');
+            $table->string('zip_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
